8df1be387e8ca285f428c832a26350da23b437d3
BrandonSchott <89819444+BrandonSchott@users.noreply.github.com>
2023-03-21 16:33:42 -0600

    Add files via upload

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,6 +16,9 @@ public class InventoryManager : MonoBehaviour
     [SerializeField]
     Inventory playerInventory, treasureInventory;
 
+    [SerializeField]
+    ItemList masterItemList;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +28,16 @@ public class InventoryManager : MonoBehaviour
 
         for (int i = 0; i < playerInventory.slots.Length; i++)
         {
-            playerInventory.slots[i] = Random.Range(0, 9);
+            playerInventory.slots[i] = Random.Range(0, masterItemList.items.Length);
+            //if first item slot in the player inventory is not a weapon then ser it to empty
+            if (i == 0 && masterItemList.items[playerInventory.slots[i]].itemType != ItemTypes.Weapon)
+            {
+                playerInventory.slots[i] = 0;
+            }
         }
         for (int i = 0; i < treasureInventory.slots.Length; i++)
         {
-            treasureInventory.slots[i] = Random.Range(0, 9);
+            treasureInventory.slots[i] = Random.Range(0, masterItemList.items.Length);
         }
         UpdateButtons();
     }
@@ -37,11 +46,11 @@ public class InventoryManager : MonoBehaviour
     {
         for (int i = 0; i < playerInventory.slots.Length; i++)
         {
-            inventoryButtons[i].GetComponentInChildren<Text>().text = playerInventory.slots[i].ToString();
+            inventoryButtons[i].GetComponentInChildren<Text>().text = masterItemList.items[playerInventory.slots[i]].itemName;
         }
         for (int i = 0; i < treasureInventory.slots.Length; i++)
         {
-            inventoryButtons[i + 9].GetComponentInChildren<Text>().text = treasureInventory.slots[i].ToString();
+            inventoryButtons[i + 9].GetComponentInChildren<Text>().text = masterItemList.items[treasureInventory.slots[i]].itemName;
         }
     }
 
@@ -64,27 +73,80 @@ public class InventoryManager : MonoBehaviour
             colors.selectedColor = savedColor;
             inventoryButtons[slot1].colors = colors;
 
+            bool canSwap = false;
+
             if (slot1 < playerInventory.slots.Length)
             {
                 temp = playerInventory.slots[slot1];
                 if (index < playerInventory.slots.Length)
                 {
-                    playerInventory.slots[slot1] = playerInventory.slots[index];
-                    playerInventory.slots[index] = temp;
+                    if (slot1 == 0 && (masterItemList.items[playerInventory.slots[index]].itemType == ItemTypes.Weapon ||
+                        masterItemList.items[playerInventory.slots[index]].itemType == ItemTypes.Empty))
+                    {
+                        canSwap = true;
+                    }
+
+                    if (slot1 != 0 && index != 0)
+                    {
+                        canSwap = true;
+                    }
+
+                    if (index == 0 && (masterItemList.items[playerInventory.slots[slot1]].itemType == ItemTypes.Weapon ||
+                        masterItemList.items[playerInventory.slots[slot1]].itemType == ItemTypes.Empty))
+                    {
+                        canSwap = true;
+                    }
+
+                    if (canSwap)
+                    {
+                        playerInventory.slots[slot1] = playerInventory.slots[index];
+                        playerInventory.slots[index] = temp;
+                    }
                 }
                 else
                 {
-                    playerInventory.slots[slot1] = treasureInventory.slots[index - 9];
-                    treasureInventory.slots[index - 9] = temp;
+                    //If playerInventory is hosen first and treasureInventory is chosen second
+                    if (slot1 == 0 && (masterItemList.items[treasureInventory.slots[index - 9]].itemType == ItemTypes.Weapon ||
+                        masterItemList.items[treasureInventory.slots[index - 9]].itemType == ItemTypes.Empty))
+                    {
+                        canSwap = true;
+                    }
+                    if (slot1 != 0 && index != 0)
+                    {
+                        canSwap = true;
+                    }
+
+                    if (canSwap)
+                    {
+                        playerInventory.slots[slot1] = treasureInventory.slots[index - 9];
+                        treasureInventory.slots[index - 9] = temp;
+                    }
+
+
                 }
             }
             else
             {
                 temp = treasureInventory.slots[slot1 - 9];
-                if(index < playerInventory.slots.Length)
+                if (index < playerInventory.slots.Length)
                 {
-                    treasureInventory.slots[slot1 - 9] = playerInventory.slots[index];
-                    playerInventory.slots[index] = temp;
+                    if(index == 0 && (masterItemList.items[treasureInventory.slots[slot1 - 9]].itemType == ItemTypes.Weapon ||
+                        masterItemList.items[treasureInventory.slots[slot1 - 9]].itemType == ItemTypes.Empty))
+                    {
+                        canSwap = true;
+                    }
+
+                    if(slot1 != 0 && index != 0)
+                    {
+                        canSwap = true;
+                    }
+
+                    if (canSwap)
+                    {
+                        treasureInventory.slots[slot1 - 9] = playerInventory.slots[index];
+                        playerInventory.slots[index] = temp;
+                    }
+
                 }
                 else
                 {
@@ -119,4 +181,38 @@ public class Inventory
         slots = new int[maxSlots];
     }
 
+}
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public ItemTypes itemType;
+
+    public Item()
+    {
+        itemName = "Empty";
+        itemType = ItemTypes.Empty;
+    }
+
+    public Item(string name, ItemTypes itemType)
+    {
+        itemName = name;
+        this.itemType = itemType;
+    }
+}
+
+public enum ItemTypes
+{
+    Empty = 0,
+    Weapon = 1,
+    Armor = 2,
+    Consumable = 3,
+    Misc = 4
+}
+
+[System.Serializable]
+public class ItemList
+{
+    public Item[] items;
 }
